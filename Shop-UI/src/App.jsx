71df28997ee0b4106eff6020be70cb132ac63880
Trường Thịnh <2059042@itec.hcmus.edu.vn>
2022-11-01 10:34:42 +0700

    update_UI

--- Shop-UI/src/App.jsx
+++ Shop-UI/src/App.jsx
@@ -4,9 +4,12 @@
 // import Login from "./pages/Login";
 // import Cart from "./pages/Cart";
 import Home from "./pages/Home";
+// import { BrowserRouter as Router, Switch,
+//   Route, Redirect,} from "react-router-dom";
 
 const App = () => {
-  return <Home/>;
+  // return <Home/>;
+  return <Home />;
 };
 
 export default App;
