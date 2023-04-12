d668358b7333e7ee29cee82994aff9f15f75b743
Trường Thịnh <2059042@itec.hcmus.edu.vn>
2023-04-12 15:22:34 +0700

    commit_full

---
+++ sa-shopping-heroku-main/admin/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyASQCZW50gPYgUMF_zmY1ssIshtd75tV9Y",
+  authDomain: "shop-14bd9.firebaseapp.com",
+  projectId: "shop-14bd9",
+  storageBucket: "shop-14bd9.appspot.com",
+  messagingSenderId: "545414285055",
+  appId: "1:545414285055:web:340ba006b8aed8a468eeca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
